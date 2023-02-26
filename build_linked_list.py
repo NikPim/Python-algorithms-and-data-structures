5105ea78098bc90e9d93cb62b1ec1ab3a7b42629
Pimkin Nikita <pimkinnikita@gmail.com>
2023-02-26 20:35:31 +0200

    adds insert and delete funcs to linked list

--- build_linked_list.py
+++ build_linked_list.py
@@ -19,6 +19,35 @@ class LinkedList:
         self.head = NewNode(value, self.head)
         self.length += 1
 
+    def insert(self, index, value):
+        new_node = NewNode(value, None)
+        if index == 0:
+            self.prepend(value)
+        elif index >= self.length:
+            self.append(value)
+        else:
+            current_node = self.traverse_to_index(index-1)
+
+            new_node.next = current_node.next
+            current_node.next = new_node
+            self.length += 1
+    
+    def remove(self, index):
+        if index == 0:
+            self.head = self.head.next
+        else:
+            current_node = self.traverse_to_index(index-1)
+            current_node.next = current_node.next.next
+        self.length -= 1
+
+    def traverse_to_index(self, index):
+        current_node = self.head
+        i = 0
+        while i != index:
+            current_node = current_node.next
+            i+=1
+        return current_node
+
     def print_val(self):
         current = self.head
         while current:
@@ -28,39 +57,28 @@ class LinkedList:
 
 
 linked_list = LinkedList(10)
-# print('10 --> None')
-# print(linked_list.head.value)
-# print(linked_list.tail.value)
-# print('10 --> 7')
-# print(linked_list.head.value)
-# print(linked_list.tail.value)
 linked_list.prepend(16)
+linked_list.append(7)
+linked_list.append(12)
+linked_list.append(17)
+linked_list.append(19)
+
 # print('16 --> 10 --> 7')
 # print(linked_list.head.value)
+# print(linked_list.head)
+# print(linked_list.head.next)
 # print(linked_list.head.next.value)
+# print(linked_list.head.next)
+# print(linked_list.head.next.next)
+# print(linked_list.head.next.next.value)
+# print(linked_list.head.next.next)
+# print(linked_list.head.next.next.next)
 # print(linked_list.tail.value)
-linked_list.append(7)
-
-print('16 --> 10 --> None')
-print(linked_list.head.value)
-print(linked_list.head)
-print(linked_list.head.next)
-print(linked_list.tail.value)
-print(linked_list.tail)
-print(linked_list.tail.next)
-
-print('16 --> 10 --> 7')
-print(linked_list.head.value)
-print(linked_list.head)
-print(linked_list.head.next)
-print(linked_list.head.next.value)
-print(linked_list.head.next)
-print(linked_list.head.next.next)
-print(linked_list.head.next.next.value)
-print(linked_list.head.next.next)
-print(linked_list.head.next.next.next)
-print(linked_list.tail.value)
-print(linked_list.tail)
-print(linked_list.tail.next)
-
+# print(linked_list.tail)
+# print(linked_list.tail.next)
 linked_list.print_val()
+linked_list.insert(2,121)
+linked_list.print_val()
+linked_list.remove(6)
+linked_list.print_val()
+print(linked_list.length)
